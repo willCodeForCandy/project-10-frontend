889194c42f492e30c9d223665a6afafebbd97414
Virginia <lavirgin@gmail.com>
2024-05-08 11:45:42 +0200

    add default user icon

--- src/Components/ProfileIcon/ProfileIcon.js
+++ src/Components/ProfileIcon/ProfileIcon.js
@@ -4,6 +4,9 @@ export const profileIcon = imgSrc => {
   return `
     <div class="profile-icon" title="Log out">
     <span class="logout-icon"><img src="/assets/logout.svg" /></span>
-    <img src="${imgSrc}" /></div>
+    <img src="${
+      imgSrc ||
+      'https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg'
+    }" /></div>
     `;
 };
